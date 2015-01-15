e1b91aca1ac3ecb721d189d6618ae62d187c3c10
meet <meet@meet.mit.edu>
2015-01-15 20:09:25 +0200

    oop

---
+++ oop.py
@@ -0,0 +1,12 @@
+class Animal:
+	def __init__(self,name,age,color,size):
+		self.name=name
+		self.age=age
+		self.color=color
+		self.size=size
+pig=Animal("nour awad",1,"green","huge")
+print(pig.name)
+print(pig.age)
+print(pig.color)
+print(pig.size)
+
